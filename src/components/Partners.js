dc20c076b58156acf8fa7d7bf18923cf548dea04
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-01-08 11:04:26 -0800

    partner logos component added

---
+++ src/components/Partners.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../myStyles.scss';
+
+const Partners = () => {
+    return (
+        <div >
+        <h3>
+            Distribution Partners
+        </h3>
+        <img className="distLogo" src='../../dist/images/medmen.png' />
+        <img className="distLogo" src='../../dist/images/Asset-1.png' />
+        <img className="distLogo" src='../../dist/images/jungle.png' />
+        </div>
+    );
+};
+
+export default Partners;
+
