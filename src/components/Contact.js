2660312cdc4f9b4cafb4d53fe9d0ecc51559da29
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-01-11 08:27:52 -0800

    contact component complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,16 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="contact" >
-        <h3>
+          <h3>
             Say Hello 
-        </h3>
-        <h1>Contact</h1>
+          </h3>
+            <h1>Contact</h1>
+              <h3>Los Angeles Office (Frogtown)</h3>
+                <p>Call Us: 1-800-555-1234</p>
+            <p>2769 Fruitdale Street</p>
+            <p>Los Angeles, CA 90039</p>
+            <p>United States</p>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3303.5351322708834!2d-118.25646738478363!3d34.10704748059101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c0d5384921d7%3A0x965e9476c202bf17!2s2769+Fruitdale+St%2C+Los+Angeles%2C+CA+90039!5e0!3m2!1sen!2sus!4v1547223675228" width="500" height="350"></iframe>
         </div>
     );
 };
