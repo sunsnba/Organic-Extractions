5e4d7ed9f2748a2bbe53eaeb23ccfa7cfd8e0eba
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-01-15 12:35:26 -0800

    cart component now fully functional

---
+++ src/components/CartPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cart from '../features/cart/index'
+
+const CartPage = (props) => {
+    return (
+        <div >
+        <h3>  Cart  </h3>
+        <Cart />
+        </div>
+    );
+};
+
+export default CartPage;
+
