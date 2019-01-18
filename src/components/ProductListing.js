964d74c76a6a649c6e1b680b23e8e0d032ab867b
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-01-17 16:19:09 -0800

    fetching product data from api

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,38 +1,53 @@
-import React from 'react';
+import React from 'react'
+import ProductListItem from './ProductListItem'
+import { connect } from 'react-redux'
 
-import ProductListItem from './ProductListItem';
-import { connect } from 'react-redux';
-import { cartItemsWithQuantities } from '../features/cart/index';
+import fetchApi from '../modules/fetchApi'
 
 // iteratre over our products and give us one product list item component per product 
-const ProductListing = (props) => {
-    return (
-            <div className="product-listing">
+class ProductListing extends React.Component {
+    componentDidMount() {
+        const { loadProducts } = this.props
+        fetchApi('get', 'https://api.myjson.com/bins/5yxb4')
+        .then((json => {
+            loadProducts(json)
+        }))
+    }
+
+    render () {
+        const { addToCart, removeFromCart, products, cart } = this.props
+
+        return  <div className="product-listing">
                 {   
-                props.products.map( product => 
+                products.map( product => 
                     <ProductListItem 
                     product={product}
-                    addToCart={props.addToCart}
-                    removeFromCart={props.removeFromCart}
-                    cartItem={props.cart.filter( cartItem => cartItem.id === product.id)[0]}
+                    addToCart={addToCart}
+                    removeFromCart={removeFromCart}
+                    cartItem={cart.filter( cartItem => cartItem.id === product.id)[0]}
                     />)
                 } 
            </div>
-    );
-};
+    }
+}
+
 // mapStateToProps defines which keys out of our redux store we're going to allow as props inside 
 // our our current component. cart key to go to the cart reducer indside of our redux store
 //
 // grabs all values for cart state and maps it to props so we can get access to values via start.cart
-function mapStateToProps (state) {
+function mapStateToProps(state) {
     return {
-        cart: state.cart
+        cart: state.cart,
+        products: state.products,
     }
 }
 // maps all of our actions to props on this component 
 // dispatches action with type: 'ADD' and payload: 'item' or same for type: 'REMOVE'
 function mapDispatchToProps (dispatch) {
     return {
+        loadProducts: (products) => {
+            dispatch({type: 'LOAD', payload: products })
+        },
         addToCart: (item) => {
             dispatch({type: 'ADD', payload: item})
         },
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import ProductListing from './ProductListing';
-import data from '../data/products.json';
 import '../myStyles.scss';
 
 const Shop = (props) => {
     return (
             <div className="shopContainer">
                 <h2>Shop</h2>
-                <ProductListing products={data.products} />
+                <ProductListing />
            </div>
     );
 };
