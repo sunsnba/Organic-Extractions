7ae9fc46ad3273b33ff856df4fb32f143c5400f0
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-01-30 16:55:27 -0800

    revised product values for fetch

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
+// const express = require('express');
+// const path = require('path');
+// const http = require('http');
 
-const app = express();
+// const app = express();
 
-// Point static path to dist
-app.use('/', express.static(path.join(__dirname, '..', 'dist')));
-app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
+// // Point static path to dist
+// app.use('/', express.static(path.join(__dirname, '..', 'dist')));
+// app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
 
-const routes = require('./routes');
+// const routes = require('./routes');
 
-app.use('/', routes);
+// app.use('/', routes);
 
-/** Get port from environment and store in Express. */
-const port = process.env.PORT || '3000';
-app.set('port', port);
+// /** Get port from environment and store in Express. */
+// const port = process.env.PORT || '3000';
+// app.set('port', port);
 
-/** Create HTTP server. */
-const server = http.createServer(app);
-/** Listen on provided port, on all network interfaces. */
-server.listen(port, () => console.log(`Server Running on port ${port}`));
+// /** Create HTTP server. */
+// const server = http.createServer(app);
+// /** Listen on provided port, on all network interfaces. */
+// server.listen(port, () => console.log(`Server Running on port ${port}`));
