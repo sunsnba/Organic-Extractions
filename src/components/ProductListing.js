29c555344d70f8183dc68a94da3217d188f3389d
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-04-03 16:23:36 -0700

    updated redux store to include customer contact info

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,63 +1,66 @@
-import React from 'react'
-import ProductListItem from './ProductListItem'
-import { connect } from 'react-redux'
+import React from "react";
+import ProductListItem from "./ProductListItem";
+import { connect } from "react-redux";
 
-import fetchApi from '../modules/fetchApi'
+import fetchApi from "../modules/fetchApi";
 
-// iteratre over our products and give us one product list item component per product 
+// iteratre over our products and give us one product list item component per product
 class ProductListing extends React.Component {
-    componentDidMount() {
-        const { loadProducts } = this.props
-        fetchApi('get', 'https://api.myjson.com/bins/5yxb4')
-        .then((json => {
-            loadProducts(json)
-        }))
-    }
+  componentDidMount() {
+    const { loadProducts } = this.props;
+    fetchApi("get", "https://api.myjson.com/bins/5yxb4").then(json => {
+      loadProducts(json);
+    });
+  }
 
-    render () {
-        const { addToCart, removeFromCart, products, cart } = this.props
+  render() {
+    const { addToCart, removeFromCart, products, cart } = this.props;
 
-        return  <div className="product-listing">
-                {   
-                products.map( product => 
-                    <ProductListItem 
-                    product={product}
-                    addToCart={addToCart}
-                    removeFromCart={removeFromCart}
-                    cartItem={cart.filter( cartItem => cartItem.id === product.id)[0]}
-                    />)
-                } 
-           </div>
-    }
+    return (
+      <div className="product-listing">
+        {products.map(product => (
+          <ProductListItem
+            product={product}
+            addToCart={addToCart}
+            removeFromCart={removeFromCart}
+            cartItem={cart.filter(cartItem => cartItem.id === product.id)[0]}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
-// mapStateToProps defines which keys out of our redux store we're going to allow as props inside 
+// mapStateToProps defines which keys out of our redux store we're going to allow as props inside
 // our our current component. cart key to go to the cart reducer indside of our redux store
 //
 // grabs all values for cart state and maps it to props so we can get access to values via start.cart
 function mapStateToProps(state) {
-    return {
-        cart: state.cart,
-        products: state.products,
-    }
+  return {
+    cart: state.cart,
+    products: state.products
+  };
 }
-// maps all of our actions to props on this component 
+// maps all of our actions to props on this component
 // dispatches action with type: 'ADD' and payload: 'item' or same for type: 'REMOVE'
-function mapDispatchToProps (dispatch) {
-    return {
-        loadProducts: (products) => {
-            dispatch({type: 'LOAD', payload: products })
-        },
-        addToCart: (item) => {
-            dispatch({type: 'ADD', payload: item})
-        },
-        removeFromCart: (item) => {
-            dispatch({ type: 'REMOVE', payload: item})
-        }
+function mapDispatchToProps(dispatch) {
+  return {
+    loadProducts: products => {
+      dispatch({ type: "LOAD", payload: products });
+    },
+    addToCart: item => {
+      dispatch({ type: "ADD", payload: item });
+    },
+    removeFromCart: item => {
+      dispatch({ type: "REMOVE", payload: item });
     }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProductListing)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductListing);
 
-// connect is a function that take these two functions, returns a new function and that new function 
+// connect is a function that take these two functions, returns a new function and that new function
 // pass to ProductListing.
--- src/features/checkout/index.js
+++ src/features/checkout/index.js
@@ -1,46 +1,47 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import Cart from '../cart/index'
-import CheckoutForm from './form'
-import fetchApi from '../../modules/fetchApi'
+import Cart from "../cart/index";
+import CheckoutForm from "./form";
+import fetchApi from "../../modules/fetchApi";
 
 function submitOrder(values, cart) {
-  const {email, name } = values.order
-  fetchApi('post', "https://student-example-api.herokuapp.com/v1/orders.json", {
-  order: {
-    name,
-    email,
-    order_items_attributes: cart.map(item => ({
-      product_id: item.id,
-      qty: item.quantity,
-    }))
-  }
-}).then(json => {
-  if(json.errors) {
-    alert('something went wrong!')
-    return
-  }
-  console.log('success');
-  document.location.href= `/orders/${json.id}`
-  })
+  const { email, name } = values.order;
+  fetchApi("post", "https://student-example-api.herokuapp.com/v1/orders.json", {
+    order: {
+      name,
+      email,
+      order_items_attributes: cart.map(item => ({
+        product_id: item.id,
+        qty: item.quantity
+      }))
+    }
+  }).then(json => {
+    if (json.errors) {
+      alert("something went wrong!");
+      return;
+    }
+    document.location.href = `/orders/${json.id}`;
+  });
 }
 
- function Checkout (props) {
-   const { cart } = props 
-  return <div>
-      <div style={{border: '1px solid black'}}> 
+function Checkout(props) {
+  const { cart } = props;
+  return (
+    <div>
+      <div style={{ border: "1px solid black" }}>
         <Cart />
       </div>
 
-      <CheckoutForm onSubmit={ (values) => submitOrder(values, cart)}/>
-  </div>
-} 
+      <CheckoutForm onSubmit={values => submitOrder(values, cart)} />
+    </div>
+  );
+}
 
 function mapStateToProps(state) {
   return {
-      cart: state.cart
-  }
+    cart: state.cart
+  };
 }
 
 export default connect(mapStateToProps)(Checkout);
